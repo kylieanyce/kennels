7daea42e98737093edaa931a6746dfceac83448f
kylieanyce <kylieanyce@gmail.com>
2021-02-26 15:06:17 -0600

    fixed location and customer card to correctly link to api

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -4,6 +4,7 @@ import "./Animal.css"
 export const AnimalCard = ({ animal }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
+        <h5 className="animal__name">{animal.breed}</h5>
         <address className="location__address">{animal.location.name}</address>
     </section>
 )
