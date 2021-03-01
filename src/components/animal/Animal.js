fc834d4e1fdbbc80ad554c4478c0beeb2ac53b5c
kylieanyce <kylieanyce@gmail.com>
2021-03-01 11:53:56 -0600

    modified animalList component so it renders animals, owners, and location for each animal

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -2,10 +2,12 @@ import React from "react"
 import "./Animal.css"
 
 //returns HTML for each prop passed in as argument
-export const AnimalCard = ({ animal }) => (
+export const AnimalCard = ({ animal, customer, location }) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
         <h5 className="animal__name">{animal.breed}</h5>
+        <h5 className="animal__name">{customer.name}</h5>
+        <h5 className="animal__name">{location.name}</h5>
         <address className="location__address">{animal.location.name}</address>
     </section>
 )
--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
-import { AnimalCard } from "./AnimalCard"
+import { LocationContext } from "../locations/LocationProvider"
+import { CustomerContext } from "../customers/CustomerProvider"
+import { AnimalCard } from "./Animal"
 import "./Animal.css"
 
 //grabs data from fetch call and sends to be rendered----------------------------
@@ -8,11 +10,16 @@ export const AnimalList = () => {
     // useContext sets the object of a var and a function and fills them
     // with whatever data is being passed from AnimalContext
     const { animals, getAnimals } = useContext(AnimalContext)
+    const { locations, getLocations } = useContext(LocationContext)
+    const { customers, getCustomers } = useContext(CustomerContext)
     // useEffect actually calls the fetch function getAnimals,
     // changing the state of animals by filling it with data
     useEffect(() => {
         console.log("AnimalList: useEffect - getAnimals")
-        getAnimals()
+        getLocations()
+            .then(getCustomers)
+            .then(getAnimals)
+            console.log(animals, locations, customers)
     }, [])
 
     //maps through animals array of data from API
@@ -21,12 +28,17 @@ export const AnimalList = () => {
             {console.log("AnimalList: Render", animals)}
             {
                 animals.map(animalObj => {
+                    const owner = customers.find(c => c.id === animalObj.customerId)
+                    const clinic = locations.find(l => l.id === animalObj.locationId)
                     //uses AnimalCard to convert each animal object to HTML
                     //the id is set to key so that each time it iterates through, a new
                     //distinct obj is returned. animal is the props attribute
                     //which is set to animalObj. AnimalCard takes in a props
                     //parameter so that's where it goes.
-                    return <AnimalCard key={animalObj.id} animal={animalObj} />
+                    return <AnimalCard key={animalObj.id}
+                        location={clinic}
+                        customer={owner}
+                        animal={animalObj} />
                 })
             }
         </div>
