f205d9b97adcd64c882ed52c1195f8076477b937
kylieanyce <kylieanyce@gmail.com>
2021-03-01 19:05:45 -0600

    edited locationlist to create form button, edited appview to route locationform

--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect } from "react"
+import { useHistory } from "react-router-dom"
 import { LocationContext } from "./LocationProvider"
+import { EmployeeContext } from "../employees/EmployeeProvider"
 import { LocationCard } from "./Location"
 import "./Location.css"
 
@@ -7,21 +9,29 @@ import "./Location.css"
 export const LocationList = () => {
     // This state changes when `getlocations()` is invoked below
     const { locations, getLocations } = useContext(LocationContext)
+    const { employees, getEmployees } = useContext(EmployeeContext)
     //useContext carries fetch call data as an object
 
     //useEffect - reach out to the world for something
     useEffect(() => {
         //calls getlocations which then populates locations with data from API 
-        getLocations()
-        
+        getEmployees()
+            .then(getLocations)
+
     }, [])
 
     //maps through locations array of data from API
     return (
         <div className="locations">
+            <button onClick={() => { history.push("/locations/create") }}>
+                Add Locations
+            </button>
             {
                 locations.map(location => {
-                    return <LocationCard key={location.id} location={location} />
+                    const worker = employees.find(w => w.id === location.locationId)
+                    return <LocationCard key={location.id} location={location} 
+                        employee={worker}
+                    />
                 })
             }
         </div>
