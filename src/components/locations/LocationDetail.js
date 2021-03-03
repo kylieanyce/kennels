c0af3144fb27fa616cbe0e987c292e86ad63dbab
kylieanyce <kylieanyce@gmail.com>
2021-03-03 15:10:23 -0600

    added locationDetail component

---
+++ src/components/locations/LocationDetail.js
@@ -0,0 +1,26 @@
+import React, { useContext, useEffect, useState } from "react"
+import { LocationContext } from "./LocationProvider"
+import "./Location.css"
+import { useParams, useHistory } from "react-router-dom"
+
+export const LocationDetail = () => {
+    const { getLocationById } = useContext(LocationContext)
+
+    const [location, setLocation] = useState({})
+    const { locationId } = useParams();
+    const history = useHistory();
+
+    useEffect(() => {
+        getLocationById(locationId)
+            .then((res) => {
+                setLocation(res)
+            })
+    }, [])
+
+    return (
+        <section className="location">
+            <h3 className="location__name">{location.name}</h3>
+            <div className="location__address">Address: {location.location?.address}</div>
+        </section>
+    )
+}
