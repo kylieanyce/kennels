71a09015da4aa71d9fc30f885ac063fb7b5b7a72
kylieanyce <kylieanyce@gmail.com>
2021-03-03 10:26:14 -0600

    added getEmployeesById component and modified employeeCard component

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -46,14 +46,11 @@ export const ApplicationViews = () => {
                         <Route path="/animal/create">
                             <AnimalForm />
                         </Route>
+                        <Route exact path="/animals/detail/:animalId(\d+)">
+                            <AnimalDetail />
+                        </Route>
                     </CustomerProvider>
                 </LocationProvider>
-            </AnimalProvider>
-
-            <AnimalProvider>
-                <Route exact path="/animals/detail/:animalId(\d+)">
-                    <AnimalDetail />
-                </Route>
             </AnimalProvider>
 
             <CustomerProvider>
