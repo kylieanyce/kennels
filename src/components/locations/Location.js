8d912cb4e584670e324dc4be089194742f961916
kylieanyce <kylieanyce@gmail.com>
2021-03-03 15:05:31 -0600

    modified location component to use routing link

--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -1,9 +1,15 @@
 import React from "react"
 import "./Location.css"
+import { Link } from "react-router-dom"
 
-export const LocationCard = ({location}) => (
-    <section className="location">
-        <h3 className="location__name">{location.name}</h3>
-        <div className="location__address">{location.address}</div>
-    </section>
-)
+export const LocationCard = ({ location }) => {
+    return (
+        <section className="location">
+            <h3 className="location__name">
+                <Link to={`/locations/detail/${location.id}`}>
+                    { location.name}
+                </Link>
+            </h3>
+        </section>
+    )
+}
